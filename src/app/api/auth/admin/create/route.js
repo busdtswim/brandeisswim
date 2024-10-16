54b94ac80cb7a2826928b440c8c0f50e8e7c8743
ericX08 <eric.xiao312@gmail.com>
2024-10-16 17:01:09 -0400

    fix imports

--- src/app/api/auth/admin/create/route.js
+++ src/app/api/auth/admin/create/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/[...nextauth]';
+import { authOptions } from '@/app/api/auth/[...nextauth]';
 
 const prisma = new PrismaClient();
 
