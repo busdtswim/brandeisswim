0c0989fffc6404e37ec65f7feca3d047c9f3bead
ericX08 <eric.xiao312@gmail.com>
2024-10-16 16:57:26 -0400

    renamed file

--- src/app/api/auth/admin/create/route.js
+++ src/app/api/auth/admin/create/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/[...nextauth]';
 
 const prisma = new PrismaClient();
 
