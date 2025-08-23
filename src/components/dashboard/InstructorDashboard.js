7953ab7bb827c5a0f65c7b3a72792196171297a5
ericX08 <eric.xiao312@gmail.com>
2025-08-23 15:30:14 -0500

    remove icon(instructor)

--- src/components/dashboard/InstructorDashboard.js
+++ src/components/dashboard/InstructorDashboard.js
@@ -416,9 +416,6 @@ const InstructorDashboard = () => {
                 <div className="flex flex-col sm:flex-row gap-3 md:gap-4">
                   {/* Search */}
                   <div className="relative">
-                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <Search className="h-4 w-4 text-gray-400" />
-                    </div>
                     <input
                       type="text"
                       placeholder="Search users or swimmers..."
